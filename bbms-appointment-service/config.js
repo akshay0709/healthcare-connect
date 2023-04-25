7edb0a576d9647272b0b18a9eb3d9aae8183f15d
Akshay Pawar <akshay070993@gmail.com>
2023-04-24 23:43:48 -0700

    Replaced dbconnection URL to point to localhost instance that will be booted in docker

--- bbms-appointment-service/config.js
+++ bbms-appointment-service/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    dburl : 'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@ds263759.mlab.com:63759/bbmsdb'
+    dburl : 'mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + 'localhost:27017/bbmsdb'
 };
